666f6ddc008ed9d1d6f72d91127b4251916e90ec
unknown <satyamsetty.eswara@gmail.com>
2020-09-08 11:39:44 +0530

    installed ejs

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app  = express();
 const port = 8000;
 
-
+//express router
 app.use('/',require('./routes'));
+
+//setting up view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
         // console.log(err); normal print
